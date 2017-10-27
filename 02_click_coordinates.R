e8fc0c985f79f6e073a3c9d7f137f942d8aca11a
riinuots <riinu.ots@gmail.com>
2017-10-27 10:09:08 +0100

    (0, 0) was top-left, not bottom-left so reversed the y coordinate on the plot.

--- 02_click_coordinates.R
+++ 02_click_coordinates.R
@@ -9,10 +9,10 @@ click_coordinates = read_csv("candyhierarchy2017.csv") %>%
   separate(coordinates, into = c("x", "y"), sep = ",") %>% 
   mutate(x = str_replace(x, "\\(", "") %>% as.numeric()) %>% 
   mutate(y = str_replace(y, "\\)", "") %>% as.numeric()) %>% 
-  mutate(panel = case_when(x<=50 & y <= 50 ~ "bottom-left",
-                           x<=50 & y > 50  ~ "top-left",
-                           x>50 & y <= 50 ~ "bottom-right",
-                           x>50 & y > 50  ~ "top-right"))
+  mutate(panel = case_when(x<=50 & y <= 50 ~ "top-left",
+                           x<=50 & y > 50  ~ "bototm-left",
+                           x>50 & y <= 50 ~ "top-right",
+                           x>50 & y > 50  ~ "bottom-right"))
 
 img <- readPNG("news_selection.png")
 g <- rasterGrob(img, interpolate=TRUE)
@@ -21,13 +21,13 @@ image_ratio = 755/586
 
 click_coordinates %>% 
   mutate(y_scaled = image_ratio*y) %>% 
-  ggplot(aes(x, y_scaled, colour = panel)) + 
+  ggplot(aes(x, -y_scaled, colour = panel)) + 
   #annotation_custom(g, xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf) +
-  annotation_custom(g, xmin=0, xmax=100, ymin=0, ymax=image_ratio*100) +
+  annotation_custom(g, xmin=0, xmax=100, ymax=0, ymin=-image_ratio*100) +
   geom_point(alpha = 0.5, shape = 4, stroke = 1) +
   # coord_cartesian should replace the scale_y and scale_x in this use, but its expand doesn't trim 2/4 sides
   #coord_cartesian(xlim = c(0, 100), ylim = c(0, image_ratio*100), expand = FALSE) +
-  scale_y_continuous(limits = c(0, image_ratio*100), expand = c(0, 0)) +
+  scale_y_continuous(limits = c(-image_ratio*100, 0), expand = c(0, 0)) +
   scale_x_continuous(limits = c(0, 100), expand = c(0, 0)) +
   coord_fixed() +
   theme_void() +
